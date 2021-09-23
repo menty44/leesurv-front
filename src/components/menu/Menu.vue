8ae9a04c5dcca5ced3c2d5c541a82571190e6ba4
Fredrick Oluoch <menty44@gmail.com>
2021-09-24 00:20:35 +0300

    more routes v2

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -9,52 +9,54 @@
       @select="handleSelect"
   >
 
-    <el-menu-item index="6"><img height="30px" src="../../assets/SRVeY.png"></el-menu-item>
-    <el-menu-item index="5">
+    <el-menu-item index="1"><img height="30px" src="../../assets/SRVeY.png"></el-menu-item>
+    <el-menu-item index="2">
       <i class="el-icon-menu"></i>
       Analytics</el-menu-item>
-    <el-menu-item index="1">
+    <el-menu-item index="3">
       <i class="el-icon-wallet"></i>
       My Awards</el-menu-item>
 
-    <el-menu-item index="3" >
+    <el-menu-item index="4" >
       <i class="el-icon-pie-chart"></i>
       Surveys</el-menu-item>
 
-    <el-menu-item index="3" >
+    <el-menu-item index="5" >
       <i class="el-icon-grape"></i>
       Users</el-menu-item>
 
-    <el-menu-item index="3" >
+    <el-menu-item index="6" >
       <i class="el-icon-user"></i>
       Profile</el-menu-item>
 
-    <el-sub-menu index="2">
+    <el-sub-menu index="7">
       <template #title>
         <i class="el-icon-setting"></i>
         Communications</template>
-      <el-menu-item index="2-1">Send Email</el-menu-item>
+      <el-menu-item index="7-1">Send Email</el-menu-item>
 
-      <el-menu-item index="2-2">Send SMS</el-menu-item>
+      <el-menu-item index="7-2">Send SMS</el-menu-item>
     </el-sub-menu>
 
-    <el-sub-menu index="4">
+    <el-sub-menu index="8">
 
       <template #title><i class="el-icon-setting"></i>
         Settings</template>
-      <el-menu-item index="2-1">Roles</el-menu-item>
-      <el-menu-item index="2-2">Permissions</el-menu-item>
-      <el-menu-item index="2-3">Menus</el-menu-item>
-      <el-menu-item index="2-3">Channels</el-menu-item>
+      <el-menu-item index="8-1">Roles</el-menu-item>
+      <el-menu-item index="8-2">Permissions</el-menu-item>
+      <el-menu-item index="8-3">Menus</el-menu-item>
+      <el-menu-item index="8-3">Channels</el-menu-item>
     </el-sub-menu>
 
-    <el-menu-item index="3" >
+    <el-menu-item index="9" @click="drawer = true">
       <i class="el-icon-message"></i>
-
       Notifications
       <el-badge :value="3" class="item"></el-badge></el-menu-item>
 
   </el-menu>
+  <el-drawer v-model="drawer" title="I am the title" :with-header="false">
+    <span>Hi there!</span>
+  </el-drawer>
 </template>
 
 <script >
@@ -62,13 +64,13 @@ import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   setup() {
-    const activeIndex = ref('1')
-    const activeIndex2 = ref('1')
+    // const activeIndex = ref('1')
+    const activeIndex2 = ref('2')
     const handleSelect = (key, keyPath) => {
       console.log(key, keyPath)
     }
     return {
-      activeIndex,
+      drawer: ref(false),
       activeIndex2,
       handleSelect,
     }
--- src/router.js
+++ src/router.js
@@ -19,6 +19,36 @@ const router = createRouter({
             component: () => import('@/components/dashboard/Dashboard'),
         },
         {
+            path: '/roles',
+            name: 'Roles',
+            component: () => import('@/components/roles/Roles'),
+        },
+        {
+            path: '/reset',
+            name: 'Reset',
+            component: () => import('@/components/reset/Reset'),
+        },
+        {
+            path: '/permissions',
+            name: 'Permissions',
+            component: () => import('@/components/permissions/Permissions'),
+        },
+        {
+            path: '/survey',
+            name: 'Survey',
+            component: () => import('@/components/survey/Survey'),
+        },
+        {
+            path: '/profile',
+            name: 'Profile',
+            component: () => import('@/components/profile/Profile'),
+        },
+        {
+            path: '/sms',
+            name: 'SMS',
+            component: () => import('@/components/sms/SMS'),
+        },
+        {
             path: '/org/login',
             name: 'Organizationlogin',
             component: () => import('@/components/organization/Organizationlogin'),
